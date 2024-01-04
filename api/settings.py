e1e19bb261fcc332db1d3e992250176621b71408
puru-reddy <puru13794@gmail.com>
2024-01-04 18:57:25 +0530

    procfile changes

--- api/settings.py
+++ api/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PORT = int(os.environ.get('PORT', 8000))
 
 
 # Quick-start development settings - unsuitable for production
